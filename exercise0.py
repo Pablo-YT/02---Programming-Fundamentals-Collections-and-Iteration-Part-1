4593583dd6830953c1f03ead3b291a8296ae1190
Josh Date <jjdate@live.com>
2019-02-17 01:20:20 -0500

    done

---
+++ exercise0.py
@@ -0,0 +1,78 @@
+#Creating Lists and Dictionaries
+#Save each of the following lists and dictionaries into variables. You will use them throughout the assignment.
+
+#Eg. fav_colours = ...
+
+#Create a list for each item below that contains the given information:
+
+#your favourite colours
+#the age of you and your siblings/cousins/friends
+#flip a coin 5 times and record whether or not the result was 'heads'
+#your three favourite performing artists
+
+#Create a dictionary for each item below that contains the given information:
+
+#three words and their definitions
+#your favourite movie names and their year of creation
+#three cities of the world and their population
+#the names of your siblings/cousins/friends and their ages
+
+#After you've done that you should run your code to make sure there aren't any syntax errors. 
+#You should get in the habit of running your code after each step in order to catch any mistakes as soon as they're introduced.
+
+
+#FAV Color
+favourite_colors = ["red" , "yellow", "black" "purple"]
+print(favourite_colors[0])
+
+#Ages
+age_fam = [17, 13, 15, 19, 10]
+
+print(ages(0))
+
+
+#flip coin lul
+
+coin_flip = ["heads", "tails" "heads", "tails", "heads"]
+
+print(coin_flip(3))
+
+#artist
+
+fav_artist = ["Billy Ray", "AMV", "The Rolling Stones"]
+
+print(fav_artist(2))
+
+#Fav Words
+three_fav_words = {
+'cover me': "watch my six"
+'AYOL' : "Are You Online"
+'Home' : "I wanna go Home"
+}
+print(three_fav_words('AYOL'))
+
+#movie
+fav_movie = {
+	'Kick Ass':"2015"
+	'Dragon Ball Super Movie': '2018'	
+	'A Movie': "2016"
+}
+
+print(fav_movie("Kick Ass"))
+
+#cities
+
+cities_population = {
+	"Toronto": "2.81 million"
+	"Brampton": "520,911 thousand"
+	"Milton":"110,128 thousand"
+}
+
+print(cities_population("Toronto"))
+
+#Family Age
+age_fam = {
+'Ray': 17, 'Norman': 13, 'Conner': 15, 'Nash': 19, 'Emma': 10
+
+ }
+print(age_fam['Ray'])
